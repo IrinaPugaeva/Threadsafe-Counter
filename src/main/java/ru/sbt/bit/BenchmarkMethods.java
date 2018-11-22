7f36449e052d226a5aad00e2f35d659c1c831192
Irina Pugaeva <pugaevaarina@gmail.com>
2018-11-22 15:22:14 +0300

    Add benchmark tests

--- src/main/java/ru/sbt/bit/BenchmarkMethods.java
+++ src/main/java/ru/sbt/bit/BenchmarkMethods.java
@@ -6,18 +6,66 @@ import ru.sbt.bit.counter.SyncCounter;
 
 public class BenchmarkMethods {
     @Benchmark
-    @BenchmarkMode(Mode.Throughput)
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
     @Fork(value = 1, warmups = 1)
     @Threads(2)
-    public int synchronizedCounter(SyncCounter counter) {
+    public int synchronizedCounterThreads2(SyncCounter counter) {
         return counter.getCurrentNumber();
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.Throughput)
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    @Fork(value = 1, warmups = 1)
+    @Threads(4)
+    public int synchronizedCounterThreads4(SyncCounter counter) {
+        return counter.getCurrentNumber();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    @Fork(value = 1, warmups = 1)
+    @Threads(8)
+    public int synchronizedCounterThreads8(SyncCounter counter) {
+        return counter.getCurrentNumber();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    @Fork(value = 1, warmups = 1)
+    @Threads(16)
+    public int synchronizedCounterThreads16(SyncCounter counter) {
+        return counter.getCurrentNumber();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
     @Fork(value = 1, warmups = 1)
     @Threads(2)
-    public int atomicCounter(ConcurrentCounter counter) {
+    public int atomicCounterThreads2(ConcurrentCounter counter) {
+        return counter.getCurrentNumber();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    @Fork(value = 1, warmups = 1)
+    @Threads(4)
+    public int atomicCounterThreads4(ConcurrentCounter counter) {
+        return counter.getCurrentNumber();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    @Fork(value = 1, warmups = 1)
+    @Threads(8)
+    public int atomicCounterThreads8(ConcurrentCounter counter) {
+        return counter.getCurrentNumber();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    @Fork(value = 1, warmups = 1)
+    @Threads(16)
+    public int atomicCounterThreads16(ConcurrentCounter counter) {
         return counter.getCurrentNumber();
     }
 }
