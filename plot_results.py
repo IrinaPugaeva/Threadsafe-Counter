ca21b8967fdc3b4fd43c302d82da285d14b29d26
Irina Pugaeva <pugaevaarina@gmail.com>
2018-11-24 20:08:44 +0000

    Plot images

--- plot_results.py
+++ plot_results.py
@@ -7,6 +7,9 @@ def plot_benchmark_results():
     df = pd.read_csv('benchmark_result.csv')
     df['Benchmark'] = df['Benchmark'].apply(lambda x: x.split('.')[4][:x.split('.')[4].find('Counter')])
     df = df.sort_values(by=['Threads'])
+    plt.ylabel(df['Unit'].unique()[0])
+    plt.xlabel('ThreadCount')
+    plt.title('Зависимость между количеством потоков и операций в микросекунду')
     for name in df['Benchmark'].unique():
         plt.plot(df[df.Benchmark == name]['Threads'], df[df.Benchmark == name]['Score'], label=name)
     plt.legend()
